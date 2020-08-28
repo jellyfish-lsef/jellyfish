d777a89761c30990f5b227920d0ebf8aec28d22e
Leo Nesfield <leo@thelmgn.com>
2020-08-28 13:51:08 +0100

    Update build-windows.bat

--- build-windows.bat
+++ build-windows.bat
@@ -1,5 +1,7 @@
 @echo [Windows] Building
 mkdir builds
-electron-packager . Jellyfish --exclude=docs --app-version 1.2.0 --protocol=jellyfish-lsef --platform win32 --out ./builds --overwrite --icon ./www/assets/icon-win.ico --asar --app-copyright "(c) 2020 theLMGN - Do not redistribute. Provided with NO warranty" --app-bundle-id com.thelmgn.jellyfish --darwin-dark-mode-support
+start electron-packager . Jellyfish --exclude=docs --app-version 1.2.0 --protocol=jellyfish-lsef --platform win32 --out ./builds --overwrite --icon ./www/assets/icon-win.ico --asar --app-copyright "(c) 2020 theLMGN - Do not redistribute. Provided with NO warranty" --app-bundle-id com.thelmgn.jellyfish --darwin-dark-mode-support
+pause
 copy ./JellyDriver-SynapseX.exe builds/Jellyfish-win32-x64/
+copy ./sxlib.dll builds/Jellyfish-win32-x64/
 copy ./JellyDriver-SirHurt.exe builds/Jellyfish-win32-x64/
