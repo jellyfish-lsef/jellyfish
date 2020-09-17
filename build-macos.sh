b943d194184a1deab85b80a738664cbbab0732d2
Leo Nesfield <leo@thelmgn.com>
2020-09-17 10:25:48 +0100

    update build scripts

--- build-macos.sh
+++ build-macos.sh
@@ -1,7 +1,7 @@
 echo "[macOS] Building"
 VERSION="$(node ./getVersion.js)"
 mkdir builds
-electron-packager . Jellyfish --exclude=docs --app-version "$VERSION" --protocol=jellyfish-lsef --platform darwin --out ./builds --overwrite --icon ./www/assets/icon.icns --asar --app-copyright "(c) 2020 theLMGN - Do not redistribute. Provided with NO warranty" --app-bundle-id com.thelmgn.jellyfish --darwin-dark-mode-support
+electron-packager . Jellyfish --exclude=docs --app-version "$VERSION" --protocol=jellyfish-lsef --platform darwin --out ./builds --overwrite --icon ../jellyfish-ui/assets/icon.icns --asar --app-copyright "(c) 2020 theLMGN - Do not redistribute. Provided with NO warranty" --app-bundle-id com.thelmgn.jellyfish --darwin-dark-mode-support
 cd ./builds
 mv Jellyfish-darwin-x64 "Jellyfish-$VERSION"
 cd "Jellyfish-$VERSION"
